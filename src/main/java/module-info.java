0fe69397188420efa8769aa37a0fcc9597db3a8c
aidan <aidan@nutmeg.com>
2017-12-18 12:09:24 +0000

    Update to allow building with Java 9

---
+++ src/main/java/module-info.java
@@ -0,0 +1,18 @@
+module petclinicx {
+    requires java.xml.bind;
+    requires javax.transaction.api;
+    requires java.validation;
+    requires hibernate.jpa;
+    requires org.hibernate.validator;
+    requires spring.beans;
+    requires spring.core;
+    requires spring.context;
+    requires spring.tx;
+    requires spring.web;
+    requires spring.webmvc;
+    requires spring.data.commons;
+    requires spring.data.jpa;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires cache.api;
+}
